17fec0323dda662ea95a840026690e9ab34c7bca
Aswin Karthik S <aswinkarthik93@gmail.com>
2015-02-12 01:59:43 +0530

    Building artifacts

--- build.sh
+++ build.sh
@@ -3,7 +3,16 @@
 set -e 
 
 echo "Starting up script"
+mkdir artifacts
+
 cd ruby-app/blog
 source /etc/profile.d/rvm.sh
+
 ruby -v
+
 bundle install
+bundle package --all
+
+cd ../
+tar blog.tar blog
+cp blog.tar ../artifacts
